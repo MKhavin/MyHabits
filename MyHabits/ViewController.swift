e9b5a6c92e7598b3a7f54221b652ae3e792a6fc4
MichaelK <havinmichael@gmail.com>
2021-12-29 23:53:09 +0300

    Create app MyHabits with LaunchScreen

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Michael Khavin on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
